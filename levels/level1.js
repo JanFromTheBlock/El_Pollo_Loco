5cbd95bc39b2c147e55fe96edd09f1787b46888b
JanFromTheBlock <133579648+JanFromTheBlock@users.noreply.github.com>
2023-11-09 16:59:52 +0100

    5 Flaschen random auf Spielfeld verteilt

--- levels/level1.js
+++ levels/level1.js
@@ -33,6 +33,10 @@ const level1 = new Level(
         new BackgroundObject('img/5_background/layers/1_first_layer/2.png', 719*3),
     ],
     [
-        new Bottle()
+        new Bottle(),
+        new Bottle(),
+        new Bottle(),
+        new Bottle(),
+        new Bottle(),
     ],
 );
--- models/bottles.class.js
+++ models/bottles.class.js
@@ -1,9 +1,9 @@
 class Bottle extends DrawableObject {
-x = 200;
-y = 200;
 height = 60;
 width = 50;
     constructor() {
         super().loadImage('img/7_statusbars/3_icons/icon_salsa_bottle.png');
-    }
+        this.x = 200 + Math.random() * 1500;
+        this.y = 150 - Math.random() * 70;
+     }
 }
